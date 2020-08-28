841ed8d1cf4d12b8fe3e1f36f1b01f6bccb128c0
Arti Singh <arti.a.singh@capgemini.com>
2020-08-28 12:03:12 +0200

    Add project files.

---
+++ WebApplication1/JsonReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace WebApplication1
+{
+    public class JsonReader
+    {
+        public string url_short { get; set; }
+        public string url_long { get; set; }
+        public int type { get; set; }
+    }
+
+
+}
